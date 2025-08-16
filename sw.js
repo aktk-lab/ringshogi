f6435967b904a35c8442c319653d708fda5042e1
aktk-lab <worldflower333@gmail.com>
2025-08-16 20:39:25 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+// GitHub Pagesでもサブパスに自動対応
+const BASE = self.registration.scope.replace(/\/$/, '');
+const CACHE_NAME = 'ringshogi-v1';
+const ASSETS = [
+  `${BASE}/`, `${BASE}/index.html`, `${BASE}/manifest.webmanifest`,
+  `${BASE}/sw.js`, `${BASE}/icons/icon-192.png`, `${BASE}/icons/icon-512.png`
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+});
+self.addEventListener('message', e=>{
+  if(e.data?.type==='SKIP_WAITING') self.skipWaiting();
+});
